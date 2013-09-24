ccc0d65c9dd399c520b3d302e642046456d2f0ac
Ivan Molodetskikh <yalterz@gmail.com>
2013-09-24 20:13:51 +0400

    Forgot to set the correct anchor property of the download button.

--- SteamChatPainter/MainForm.Designer.cs
+++ SteamChatPainter/MainForm.Designer.cs
@@ -223,6 +223,7 @@
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.Location = new System.Drawing.Point(12, 486);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 23);
